e2faa603951af0593dd04472baea5cf83b8959e3
Samuel Pérez <fernando.samuelperez01@gmail.com>
2023-10-07 19:25:14 -0600

    Add 404 Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductList from "./pages/ProductsDashboard";
 import ProductForm from "./pages/Create-Product-Page";
 import DishData from "./pages/Dish-Data";
 import EditPage from "./pages/Edit-Page";
+import NotFoundPage from "./pages/404";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/create-product" element={<ProductForm />} />
         <Route path="/products/:id" element={<DishData />} />
         <Route path="/products/edit-product/:id" element={<EditPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
